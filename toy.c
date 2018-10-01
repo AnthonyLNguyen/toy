b39bbbe6c834dd3398aa62ea9ca11b76331833c1
thingon <anthonylnguyen97@gmail.com>
2018-09-30 17:11:25 -0700

    allow printing of tokens

--- toy.c
+++ toy.c
@@ -5,7 +5,10 @@ extern int yylex();
 extern int yylineno;
 extern char* yytext;
 
+
 char *names[] = {NULL, "_boolean", "_break", "_class", "_double", "_else", "_extends", "_for", "_if", "_implements", "_int", "_interface", "_new", "_newarray", "_null", "_println", "_readln", "_return", "_string", "_void", "_while", "_plus", "_minus", "_multiplication", "_division", "_mod", "_less", "_lessequal", "_greater", "_greaterequal", "_equal", "_notequal", "_and", "_or", "_not", "_assignop", "_semicolon", "_comma", "_period", "_leftparen", "_rightparen", "_leftbracket", "_rightbracket", "_leftbrace", "_rightbrace", "_intconstant", "_doubleconstant", "_stringconstant", "_booleanconstant", "_id" };
+
+char *tokens[] = {NULL, "boolean", "break", "class", "double", "else", "extends", "for", "if", "implements", "int", "interface", "new", "newarray", "null", "println", "readln", "return", "string", "void", "while", "plus", "minus", "multiplication", "division", "mod", "less", "lessequal", "greater", "greaterequal", "equal", "notequal", "and", "or", "not", "assignop", "semicolon", "comma", "period", "leftparen", "rightparen", "leftbracket", "rightbracket", "leftbrace", "rightbrace", "intconstant", "doubleconstant", "stringconstant", "booleanconstant", "id" };
 
 int main(void)
 {
@@ -13,7 +16,8 @@ int main(void)
 
 	ntoken = yylex();
 	while(ntoken) {
-		printf("%d\n", ntoken);
+		//printf("%d", ntoken);
+		printf("%s ",tokens[ntoken]);
 		ntoken = yylex();
 	}
 	return 0;
